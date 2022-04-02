8dbc2b0a1986028b6ea9286bdab0901ebc516458
Dtopiast <im_satoshi@musk.com>
2022-04-02 17:11:13 -0500

    Add source code v1.0

---
+++ Konphil-source/Models/NotificationModels/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konphil.Models.NotificationModels
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public NotificationType Type { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+     public enum NotificationType
+    {
+        Deal,
+        SuperDeal,
+        Catch,
+
+    }
+}
---
+++ Konphil-source/Models/SearchModels/Search.cs
@@ -0,0 +1,18 @@
+﻿//using SQLite;
+using Konphil.Models.MarketModels;
+using Konphil.Models.ArticleModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace Konphil.Models.SearchModels
+{
+    public class Search
+    {
+       // [PrimaryKey, AutoIncrement]
+        public int SearchId { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)*$", ErrorMessage = "Invalid Article")]
+        public string Article { get; set; }
+        public Market Market { get; set; }
+        public ArtciclesOrder Order { get; set; }
+        public int Page { get; set; }
+    }
+}
